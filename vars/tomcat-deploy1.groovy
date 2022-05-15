46ce2c1b4835e8866de95bad705649e6051d8a99
Kushikumar <103350297+Kushikumar@users.noreply.github.com>
2022-05-15 13:30:05 +0530

    Update tomcat-deploy1.groovy

--- vars/tomcat-deploy1.groovy
+++ vars/tomcat-deploy1.groovy
@@ -1,5 +1,5 @@
 def call(tomcatIP,warName,credID"){     
-     ssh agent([${"credID"}]) {
+     sshagent([${"credID"}]) {
                sh "scp -o StrictHostKeyChecking=no target/*.war ec2-user@${tomcatIP}:/opt/tomcat8/webapps/${warName}.war"
                sh "ssh ec2-user@${tomcatIP} /opt/tomcat8/bin/shutdown.sh"
                sh "ssh ec2-user@&{tomcatIP} /opt/tomcat8/bin/startup.sh"
